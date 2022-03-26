ea5469175b808080cea1b071800facc4d9107797
Noku <nokuwastakenwastaken@gmail.com>
2022-03-26 22:23:30 +0100

    init

---
+++ setup-git.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Noku"
+git config --global user.email "nokuwastakenwastaken@gmail.com"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
